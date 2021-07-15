afd86043b70dc233b64736c37f97506cd10aa22e
nagano.masami <nagano.masami.biz@gmail.com>
2021-07-15 15:06:39 +0900

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,35 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  def follow(other_user)
+    #フォローしようとしているother_userが自分自身でないかを検査
+    unless self == other_user
+      #user.follow(other)を実行したとき、userが代入される
+      #Userのインスタンスがself
+      #見つかればRelationshipモデルのインスタンスを返す
+      #見つからなければ、フォロー県警を保存(create=build+save)
+      #これにより、すでにフォローされている場合にフォローが重複して保存されることがなくなる
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  #フォローがあればアンフォローする
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    #relationshipが存在すればdestroy
+    relationship.destroy if relationship
+  end
+
+  #self.followingsによりフォローしているUser達を取得し、
+  def following?(other_user)
+    #other_userが含まれないかを確認
+    #含まれている場合には、ture
+    #含まれてない場合には、false
+    self.followings.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,20 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  #中間テーブルから先にある、フォロー中のユーザとフォローされているユーザ一覧を表示するベージ
+  #そのためのルーティングが下記
+  resources :users, only: [:index, :show, :create] do
+    member do
+      #:idを持つユーザのフォローユーザ一覧を表示する機能をControllerでつける
+      get :followings
+      get :followers
+    end
+    collection do
+      get :search
+    end
+  end
 
   resources :microposts, only: [:create, :destroy]
+  #ログインユーザがユーザをフォロー・アンフォローできるようにするルーティング
+  resources :relationships, only: [:create, :destroy]
 end
